67b9203ef7386d125f57f461b099354291e78b4a
Itou-Hiroaki-qqq <chiteijin315@gmail.com>
2025-06-23 18:39:02 +0900

    section voicesのhtml,css作成　JavaScript作成

--- assets/script/main.js
+++ assets/script/main.js
@@ -1,6 +1,43 @@
 document.addEventListener('DOMContentLoaded', () => {
 
-  // ◆◆◆section qa のアコーディオンメニュー◆◆◆
+  // ◆◆◆section voices のカルーセル制御◆◆◆
+  // Swiperの初期化
+    const swiper = new Swiper('.swiper', {
+
+        // ループ設定
+        loop: false,  //ループによるスライド個数のエラーを防ぐため
+
+        // ページネーション設定
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+        },
+
+        // sp設定
+        slidesPerView: 1,
+        slidesPerGroup: 1, //クリック時に進むスライド数
+        spaceBetween: 10,
+
+        // ブレークポイント（PCサイズ以上でスライド3枚に）
+        breakpoints: {
+            768: { // 768px以上の画面幅
+                slidesPerView: 3,
+                slidesPerGroup: 3,
+                spaceBetween: 20,
+            }
+        },
+
+        // ナビゲーションボタン設定
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+
+    });
+
+
+
+  // ◆◆◆section qa のアコーディオンメニュー制御◆◆◆
   // クリック領域をli全体にするためにqa__itemを取得
   const qaItems = document.querySelectorAll('.qa__item');
 
